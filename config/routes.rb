1d73c7335dbb8b7024626ae33834b91ca4c3c981
Kyle Loveless <lovelessk999@gmail.com>
2019-09-28 19:14:56 +0000

    Revert "Allow Course Creation"
    This reverts commit 2785141d6deda0f4931c0f623847e83f5aa3a047.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-
-  # hookup for the instructor controller
-
-  namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
-  end
-
 end
