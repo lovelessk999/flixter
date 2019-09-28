2785141d6deda0f4931c0f623847e83f5aa3a047
Kyle Loveless <lovelessk999@gmail.com>
2019-09-28 18:47:28 +0000

    Allow Course Creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+
+  # hookup for the instructor controller
+
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
+
 end
