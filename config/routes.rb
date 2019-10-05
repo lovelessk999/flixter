0fae30482cd9bc09aaa0aa6719b4d70c4b569d88
Kyle Loveless <lovelessk999@gmail.com>
2019-10-05 03:29:08 +0000

    27 Student Enrollment

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :lessons, only: [:update]
-     resources :sections, only: [:update] do
+    resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
