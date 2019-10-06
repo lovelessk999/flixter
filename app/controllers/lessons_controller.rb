8fcdabc0a46436afcc6066331414471e204541eb
Kyle Loveless <lovelessk999@gmail.com>
2019-10-06 20:23:09 +0000

    28 Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,26 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+
   def show
   end
 
 
 
  private
+
+ def require_enrolled_for_current_course
+       if ! current_user.enrolled_in?(current_course)
+           redirect_to course_path(current_lesson.section.course), alert: 'Please enroll to view course details.'
+       else
+           
+       end
+    end
+
+    helper_method :current_course
+    def current_course
+        current_lesson.section.course
+    end
 
   helper_method :current_lesson
   def current_lesson
