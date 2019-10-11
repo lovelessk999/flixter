97d0963af795378595cd656e46532589e111f0de
Kyle Loveless <lovelessk999@gmail.com>
2019-10-11 17:16:00 +0000

    Lesson 34: routes broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resource :dashboard, only: [:show]
+
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
-    resources :enrollements, only: :create
+    resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
+  
   namespace:instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update]
+
     resources :sections, only: [] do
       resources :lessons, only:[:new, :create]
     end
+
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create] 
     end
   end
+
 end
